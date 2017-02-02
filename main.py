4e7ecb3b6a54a27aa35ff5b7acde54a53c207a60
Rodney Wells <rodneywells01@gmail.com>
2017-02-02 13:19:28 -0500

    Built expression tree builder class. Simplified code.

--- main.py
+++ main.py
@@ -12,52 +12,21 @@ operator_precedence = {
 
 result = []
 
-def postfix_convert(infix):
-    stack = []
-    postfix = []
-
-    for char in infix:
-        if char not in operator_precedence:
-            postfix.append(char)
-        else:
-            if len(stack) == 0:
-                stack.append(char)
-            else:
-                if char == "(":
-                    stack.append(char)
-                elif char == ")":
-                    while stack[len(stack) - 1] != "(":
-                        postfix.append(stack.pop())
-                    stack.pop()
-                elif operator_precedence[char] > operator_precedence[stack[len(stack) - 1]]:
-                    stack.append(char)
-                else:
-                    while len(stack) != 0:
-                        if stack[len(stack) - 1] == '(':
-                            break
-                        postfix.append(stack.pop())
-                    stack.append(char)
-
-    while len(stack) != 0:
-        postfix.append(stack.pop())
-
-    return postfix
-
-
 class Node(object):
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-
 class ExressionTree(object):
 
     def __init__(self, root=None):
+        # Field values
         self.__root = root
         self.postorder_result = []
         self.preorder_result = []
         self.inorder_result = []
+
 
     """
     Generate Order Trees
@@ -77,7 +46,6 @@ class ExressionTree(object):
     """
     Recursive Order helper functions
     """
-
     def __inorder_helper(self, node):
         if node:
             self.__inorder_helper(node.left)
@@ -99,69 +67,99 @@ class ExressionTree(object):
     """
     Getters for Order Results
     """
-
     def get_postorder_result(self):
-        if self.postorder_result == []:
+        if self.postorder_result is False:
             self.generate_postorder()
-
         return self.postorder_result
 
     def get_preorder_result(self):
-        if self.preorder_result == []:
+        if self.preorder_result is False:
             self.generate_preorder()
-
         return self.preorder_result
 
     def get_inorder_result(self):
-        if self.inorder_result == []:
+        if self.inorder_result is False:
             self.generate_inorder()
-
         return self.inorder_result
 
 
+class ExpressionTreeBuilder(object):
+    def __init__(self):
+        pass
 
-def create_expression_tree(infix):
-    infix = "".join(infix.split())
-    postfix = postfix_convert(infix)
+    def create_expression_tree(self, infix):
+        infix = "".join(infix.split())
+        postfix = self.postfix_convert(infix)
 
-    stack = []
+        stack = []
 
-    for char in postfix:
-        if char not in operator_precedence:
-            node = Node(char)
-            stack.append(node)
-        else:
-            node = Node(char)
-            right = stack.pop()
-            left = stack.pop()
-            node.right = right
-            node.left = left
-            stack.append(node)
+        for char in postfix:
+            if char not in operator_precedence:
+                node = Node(char)
+                stack.append(node)
+            else:
+                node = Node(char)
+                right = stack.pop()
+                left = stack.pop()
+                node.right = right
+                node.left = left
+                stack.append(node)
 
-    return ExressionTree(stack.pop())
+        return ExressionTree(stack.pop())
+
+    def postfix_convert(self, infix):
+        stack = []
+        postfix = []
+
+        for char in infix:
+            if char not in operator_precedence:
+                postfix.append(char)
+            else:
+                if len(stack) == 0:
+                    stack.append(char)
+                else:
+                    if char == "(":
+                        stack.append(char)
+                    elif char == ")":
+                        while stack[len(stack) - 1] != "(":
+                            postfix.append(stack.pop())
+                        stack.pop()
+                    elif operator_precedence[char] > operator_precedence[stack[len(stack) - 1]]:
+                        stack.append(char)
+                    else:
+                        while len(stack) != 0:
+                            if stack[len(stack) - 1] == '(':
+                                break
+                            postfix.append(stack.pop())
+                        stack.append(char)
+
+        while len(stack) != 0:
+            postfix.append(stack.pop())
+
+        return postfix
+
 
 def run_tests():
-    print "In Order:"
-    create_expression_tree("(A+B)*6").generate_inorder()
-    #print "Post Order:"
-    #create_expression_tree("(A+B)*6").postorder()
-    print "Pre Order:"
-    create_expression_tree("(A+B)*6").generate_preorder()
+    builder = ExpressionTreeBuilder()
 
     print "In Order:"
-    create_expression_tree("(A+B+C+D)/4").generate_inorder()
+    builder.create_expression_tree("(A+B)*6").generate_inorder()
+
+    print "Pre Order:"
+    builder.create_expression_tree("(A+B)*6").generate_preorder()
+
+    print "In Order:"
+    builder.create_expression_tree("(A+B+C+D)/4").generate_inorder()
     print "Post Order:"
-    exprtree = create_expression_tree("(A+B+C+D)/(4)")
+    exprtree = builder.create_expression_tree("(A+B+C+D)/(4)")
     exprtree.generate_postorder()
 
-
     print "Pre Order:"
-    create_expression_tree("(A+B+C+D)/4").generate_preorder()
-
+    builder.create_expression_tree("(A+B+C+D)/4").generate_preorder()
 
     print "Result:"
-    #print ExressionTree.get_postorder_result()
-
     print exprtree.get_postorder_result()
 
+
 def wells_tests():
+    pass
