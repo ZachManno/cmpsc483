0ef6ca83b45a540411970979998132bf1c69d8d9
Joshua <joshmamarini@gmail.com>
2017-03-01 11:39:59 -0500

    The barebones input reader.

--- readin.py
+++ readin.py
@@ -3,16 +3,28 @@
 import sys
 import getopt
 
-print(sys.argv)
 def readin():
     ### save each line of input to a list of lines, output them when the next input is an empty line
     ### print all previous lines before prompting for next line
     ### in future: add delete feature?
     lines=[]
+    doread(lines)
+    print("Your code input:")
+    i=0
+    while i<len(lines):
+        print(lines[i])
+        i=i+1
+    return lines
+
+def doread(lines):
     next=input("Input next line of code (hit ENTER to finish)\n")
+    if len(next)==0:
+        return
     lines.append(next)
-    print(lines)
-
-
+    i=0
+    while i<len(lines):
+        print(lines[i])
+        i=i+1
+    doread(lines)
 
 readin()
