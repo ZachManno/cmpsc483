5b6333efe2043ec1d2fc818d0392262b46c7e2a3
Rodney Wells <rodneywells01@gmail.com>
2017-04-17 23:55:49 -0400

    Big revisions to testing.py equation parser

--- testing.py
+++ testing.py
@@ -4,23 +4,37 @@ import introdata
 
 class EnglishProblemGenerator(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, equation):
+        """
+        Generate english problems based on a math equation.
+        :param equation: Equation input as a string
+        """
+
+        # Save strrep of equation for later.
+        self.strequation = equation
+        if type(equation) != "str":
+            raise TypeError("Expected string for equation, received " + type(equation))
+
+        # Format equation
+        self.formatequation = tagassigner.Equation(self.strequation)
+
+        # Generate dict tree representation.
+        self.equationdict = self.generate_dict_tree(self.formatequation, 0, None)
+        self.initialproblemtype = newthemeclass.get_random_type()
 
     def debug_type_test(self):
+        #TODO Remove
         myclass = "CITY"
         myclassobject = newthemeclass.str_to_class("newthemeclass", myclass)
         print(type(myclassobject))
         print(myclassobject.instanceTitle)
 
-
     def debug_display_contents(self, equationdict):
+        #TODO Remove
         for entry in equationdict:
             print(str(entry) + ": " + str(equationdict[entry][0].attribute) + " " + str(equationdict[entry][1]) + " " +
                   str(equationdict[entry][2]))
 
-
-    # PROBLEM GENERATOR METHODS
     def generate_dict_tree(self, node, myid, parent=None):
         """
         Recursively generate the tree reference structure.
@@ -49,19 +63,15 @@ class EnglishProblemGenerator(object):
         # Pass up dict tree to parent.
         return dict_tree
 
-
     def get_term(self, nodeid, equationdict):
         return equationdict[nodeid][0]
-
 
     def get_parent_id(self, nodeid, equationdict):
         # Node id: Node, Parentid, child list
         return equationdict[nodeid][1]
 
-
     def get_children(self, nodeid, equationdict):
         return equationdict[nodeid][2]
-
 
     def combine_subprob(self, begin, node, entity, end):
         subproblem = begin
@@ -69,7 +79,6 @@ class EnglishProblemGenerator(object):
 
         # Get subproblem title based on plurality.
         subproblem += " " + entity.getInstanceTitle(int(node.attribute != "1"))
-
         subproblem += end
 
         if subproblem[len(subproblem) - 1] != ".":
@@ -77,107 +86,95 @@ class EnglishProblemGenerator(object):
 
         return subproblem
 
-
     def issign(self, text):
         return text == "+" or text == "-" or text == "*" or text == "/"
 
+    def generate_problem_for_equation(self):
+        """Generate initial statements. Determine type."""
+        self.ultimatefinalproblem = ""
+        self.problemtype = newthemeclass.get_random_type()
+        self.problemobject = newthemeclass.str_to_class("newthemeclass", self.initialproblemtype)
 
-    def generate_problem_for_equation(self, equation):
-        ultimatefinalproblem = ""
-
-        # Format equation
-        formatequation = tagassigner.Equation(equation)
-
-        # Generate dict tree representation.
-        equationdict = self.generate_dict_tree(formatequation, 0, None)
 
         # Find the leftmost node.
         nodeid = 0
-        while len(equationdict[nodeid][2]) > 0:
-            nodeid = equationdict[nodeid][2][0]
+        while len(self.equationdict[nodeid][2]) > 0:
+            nodeid = self.equationdict[nodeid][2][0]
 
         # Generate a humble introduction.
-        ultimatefinalproblem += introdata.generate_intro()
-        initialproblemtype = newthemeclass.get_random_type()
+        self.ultimatefinalproblem += introdata.generate_intro()
+        print("The topic is " + self.initialproblemtype)
 
-        # initialproblemtype = "ANIMALS"
-        print("The topic is " + initialproblemtype)
+        self.ultimatefinalproblem += self.get_term(nodeid, self.equationdict).attribute
+        self.ultimatefinalproblem += " "
 
-        # Chose "big type" for problem.
-
-        initialobject = newthemeclass.str_to_class("newthemeclass", initialproblemtype)
-
-        # ultimatefinalproblem += " "
-        ultimatefinalproblem += self.get_term(nodeid, equationdict).attribute
-        ultimatefinalproblem += " "
-
-        if self.get_term(nodeid, equationdict).attribute == "1":
-            ultimatefinalproblem += initialobject.getInstanceTitle(0)
+        rootattribute = self.get_term(nodeid, self.equationdict).attribute
+        if rootattribute == "1":
+            self.ultimatefinalproblem += self.problemobject.getInstanceTitle(0)
         else:
-            ultimatefinalproblem += initialobject.getInstanceTitle(1)
+            self.ultimatefinalproblem += self.problemobject.getInstanceTitle(1)
+        self.ultimatefinalproblem += ". "
 
-        ultimatefinalproblem += ". "
+        # Initiate recursion to generate problem.
+        self.gen_on_datatype(rootattribute, 0)
 
-        # Keep building the problem based on what we see.
-        previousobjecttype = initialproblemtype
-        previousobject = initialobject
-        mulchain = False
+        # Conclude problem.
+        self.ultimatefinalproblem += introdata.generate_conclusion(self.problemtype)
 
-        nodeid += 1
-        while nodeid < max(equationdict) + 1:
-            if equationdict[self.get_parent_id(nodeid, equationdict)][0].attribute == "+":
-                # Plus chain.
-                themeclass = newthemeclass.str_to_class("newthemeclass", initialproblemtype)
-                attribute = self.get_term(nodeid, equationdict).attribute
+        return self.ultimatefinalproblem
 
-                if attribute == "+":
-                    intro = introdata.get_and_connector() + introdata.generate_intro().lower()
-                    ultimatefinalproblem += self.combine_subprob(intro, self.get_term(nodeid, equationdict), themeclass, "")
+    def gen_on_datatype(self, attribute, childid):
+        """Fork in problem generation based on subproblem attribute type."""
+        if attribute == "+":
+            self.gen_addition_helper(childid)
+        elif attribute == "*":
+            self.gen_mul_helper(childid)
+        elif attribute == "/":
+            self.gen_div_helper(childid)
 
-            if equationdict[self.get_parent_id(nodeid, equationdict)][0].attribute == "*":
-                # We want to iterate through all of the multerms.
-                continue
+    def gen_addition_helper(self, parentid):
+        """Generate addition type subproblem based on parentid"""
+        for childid in self.equationdict[parentid][2]:
 
-            ultimatefinalproblem += " "
+            # Check attribute for sign, to determine path.
+            attribute = self.equationdict[childid][0].attribute
+            if self.issign(attribute):
+                self.gen_on_datatype(attribute, childid)
+            else:
+                # Attribute is a variable. Update problem text based on addition connectors.
+                themeobject = newthemeclass.str_to_class("newthemeclass", self.problemtype)
 
-            nodeid += 1
+                intro = introdata.get_and_connector() + introdata.generate_intro().lower()
+                self.ultimatefinalproblem += self.combine_subprob(intro, self.get_term(childid, self.equationdict),
+                                                                  themeobject, "")
 
-        # TODO Assuming counting for now.
+                self.ultimatefinalproblem += " "
 
-        ultimatefinalproblem += introdata.generate_conclusion(initialproblemtype)
+    def gen_mul_helper(self, parentid):
+        """Generate multiplilcation type subproblem based on parentid"""
 
-        return ultimatefinalproblem
+        # Generate encapsulating terms for each mul piece.
+        containerlist = []
+        mulproblemtype = self.problemtype
+        mulsubproblemstring = ""
+        for dummy_idx in self.equationdict[parentid][0][2]:
+            # Generate new theme object based on mulproblemtype
+            themeobject = newthemeclass.str_to_class("newthemeclass", mulproblemtype)
 
-    def generate_problem_for_equation_r(self):
+            # Fetch a containing theme type for next object
+            # mulproblemtype = get_up_relation(themeobject) #TODO: ????
+
+            containerlist.insert(0, mulproblemtype)
+
+
+        # Using container list, generate problem definition.
+        for idx in range(len(self.equationdict[parentid][0][2])):
+            multermid = self.equationdict[parentid][0][2][idx]
+            problemtypeobject = containerlist[idx]
+
+        self.ultimatefinalproblem += mulsubproblemstring
+        pass #TODO Continue working
+
+    def gen_div_helper(self, parentid):
+        """Generate division type subproblem based on parentid"""
         pass
-
-
-def run_tests():
-    """
-    Run a series of tests to display equation output
-    """
-
-    equations = [
-        # "a + b",
-        "a + b + c",
-        # "1 + 1 + b",
-        # "a * b * c",
-        # "a + b * c",
-        # "a / b + c",
-        # "c * g"
-    ]
-
-    for equation in equations:
-        print("Original equation:")
-        print(equation)
-
-        print()
-        print("Generated Problem:")
-        # print(generate_problem_for_equation(equation))
-
-        print("=============")
-        print()
-        print()
-
-# for dummy_idx in range(3):
-#     run_tests()
--- wellstests.py
+++ wellstests.py
@@ -7,6 +7,39 @@ import testing
 # inputgenerator =
 # mysubstitutedinput = substitutor.Substitutor().finalEquation
 
-mysubstitutedinput = "1 + a*b*c"
+mysubstitutedinput = "a + b + c"
 print("Lessgooo")
+generator = testing.EnglishProblemGenerator();
+print(generator.generate_problem_for_equation(mysubstitutedinput))
 # print(testing.generate_problem_for_equation(mysubstitutedinput))
+
+
+def run_tests():
+    """
+    Run a series of tests to display equation output
+    """
+
+    equations = [
+        # "a + b",
+        "a + b + c",
+        # "1 + 1 + b",
+        # "a * b * c",
+        # "a + b * c",
+        # "a / b + c",
+        # "c * g"
+    ]
+
+    for equation in equations:
+        print("Original equation:")
+        print(equation)
+
+        print()
+        print("Generated Problem:")
+        # print(generate_problem_for_equation(equation))
+
+        print("=============")
+        print()
+        print()
+
+# for dummy_idx in range(3):
+#     run_tests()
