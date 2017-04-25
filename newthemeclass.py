e533a59d180c1b26fb76db56bdade5ae9fb5a7bb
Zach Manno <zdm5045@psu.edu>
2017-04-24 21:00:40 -0400

    more data changes again, again, again

--- newthemeclass.py
+++ newthemeclass.py
@@ -190,6 +190,7 @@ class STATES(Noun_object):
                     'CARS',
                     'PEOPLE',
                     'ANIMALS',
+                    'PLANTS',
                     'PARKS'
                 ]
         }
